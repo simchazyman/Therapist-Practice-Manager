923489bd4d75b5771694db1d67c937fd0bb7a5ad
simchazyman <simchazyman@gmail.com>
2025-07-17 16:06:46 -0400

    Added example config page.

---
+++ app/config.py
@@ -0,0 +1,12 @@
+# Google OAuth configuration
+CLIENT_ID = "your-google-oauth-client-id.apps.googleusercontent.com"
+CLIENT_SECRET = "your-google-oauth-client-secret"
+REDIRECT_URI = "https://yourdomain.com/oauth2callback"
+
+# MongoDB configuration
+MONGO_URI = "mongodb+srv://username:password@cluster0.mongodb.net/mydatabase?retryWrites=true&w=majority"
+DB_NAME = "your_database_name"
+USER_COLLECTION = "users"
+
+# List of administrator emails allowed full access
+PROTECTED_ADMINS = ["admin@example.com"]
